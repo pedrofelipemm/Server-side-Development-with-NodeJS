f518aeea748a1d2bcaca59ddc42474f2b280fdd1
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2016-05-15 15:08:08 -0300

    week1/Assignment

---
+++ Assignment1/README.md
@@ -0,0 +1,1 @@
+# Server-side-Development-with-NodeJS
---
+++ Assignment1/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter.js');
+var leaderRouter = require('./leaderRouter.js');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+app.use(morgan('dev'));
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
+
+app.listen(port, hostname, function () {
+  console.log('Server running at http://${hostname}:${port}/')
+});
