3ebd5091dfa659e2d0f7c400998142f041935a26
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2016-05-22 19:09:38 -0300

    week3/Exercise5

---
+++ rest-server-passport/models/dishes.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var commentSchema = new Schema({
+    rating:  {type: Number, min: 1, max: 5, required: true},
+    comment:  {type: String, required: true},
+    author:  {type: String, required: true}
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Schema({
+    name: {type: String, required: true, unique: true},
+    image: {type: String, required: true, unique: true},
+    category: {type: String, required: true, unique: false},
+    label: {type: String, required: false, default: ""},
+    price: {type: Currency, required: true, unique: false},
+    description: {type: String, required: true},
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
